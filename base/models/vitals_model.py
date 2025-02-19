905f12077c6eaed95f4c9d6f4fb360d9c5db9948
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-19 11:42:15 +0200

    feat[models]: add baseline property

--- base/models/vitals_model.py
+++ base/models/vitals_model.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class PaticipantVitals(models.Model):
 
     participant_identifier = models.CharField(max_length=50)
     height = models.FloatField()
     blood_pressure = models.CharField(max_length=50)
     weight = models.FloatField()
+    baseline = models.BooleanField(default=True)
