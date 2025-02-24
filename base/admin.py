0ba0a2a5cec046e309a8c2ae2ff1564f095844d6
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 14:51:31 +0200

    register model

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models.vitals_model import ParticipantVital
 from .models.adverse_event_model import AdverseEvent
 from .models.blood_samples_models import BloodSample
+from .models.compliance_form_model import ComplianceForm
 # Register your models here.
 
 admin.site.register(ParticipantVital)
 admin.site.register(AdverseEvent)
 admin.site.register(BloodSample)
+admin.site.register(ComplianceForm)
