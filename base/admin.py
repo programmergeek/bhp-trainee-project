ebad4909000e71753cf18a882360acec3a8af512
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 15:20:45 +0200

    feat: add choices on severity

--- base/admin.py
+++ base/admin.py
@@ -3,9 +3,11 @@ from .models.vitals_model import ParticipantVital
 from .models.adverse_event_model import AdverseEvent
 from .models.blood_samples_models import BloodSample
 from .models.compliance_form_model import ComplianceForm
+from .models.exit_interview_model import ExitInterview
 # Register your models here.
 
 admin.site.register(ParticipantVital)
 admin.site.register(AdverseEvent)
 admin.site.register(BloodSample)
 admin.site.register(ComplianceForm)
+admin.site.register(ExitInterview)
--- base/models/final_questionnaire_model.py
+++ base/models/final_questionnaire_model.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class FinalQuestionnaire(models.Model):
 
+    CHOICES = (('unwilling', 'Unwilling'), ('moderatly_willing',
+               'Moderate Willing'), ('very_willing', 'Very Willing'))
+
     participant_identifier = models.CharField(max_length=50)
-    participant_experience = models.CharField()
-    perceived_benefits = models.CharField()
-    perceived_challenges = models.CharField()
-    willingness_to_participate_in_future_studies = models.CharField()
+    participant_experience = models.CharField(max_length=500)
+    perceived_benefits = models.CharField(max_length=500)
+    perceived_challenges = models.CharField(max_length=500)
+    willingness_to_participate_in_future_studies = models.CharField(
+        max_length=20, choices=CHOICES)
