74ad913401bb9d4ccf8bf6064940a00bda5ebd7c
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 15:22:38 +0200

    feat: register final questionnaire model

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,7 @@ from .models.adverse_event_model import AdverseEvent
 from .models.blood_samples_models import BloodSample
 from .models.compliance_form_model import ComplianceForm
 from .models.exit_interview_model import ExitInterview
+from .models.final_questionnaire_model import FinalQuestionnaire
 # Register your models here.
 
 admin.site.register(ParticipantVital)
@@ -11,3 +12,4 @@ admin.site.register(AdverseEvent)
 admin.site.register(BloodSample)
 admin.site.register(ComplianceForm)
 admin.site.register(ExitInterview)
+admin.site.register(FinalQuestionnaire)
