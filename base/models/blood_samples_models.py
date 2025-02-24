fd04e2565c4fc240a879c1252501e7d0d9e17ad5
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 14:48:35 +0200

    fix spelling mistake and register model

--- base/models/blood_samples_models.py
+++ base/models/blood_samples_models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class BloodSamples(models.Model):
+
+class BloodSample(models.Model):
 
     participant_identifier = models.CharField(max_length=50)
-    sample_id = models.CharField(max_legnth=20)
+    sample_id = models.CharField(max_length=20)
     collection_date = models.DateTimeField()
     requested_test = models.CharField(max_length=256)
     phlebotomist_signature = models.CharField(max_length=256)
