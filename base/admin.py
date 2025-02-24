10b8132287c30aca43770637cd71a8ba96c7462d
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 12:37:23 +0200

    register adverse event model

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models.vitals_model import ParticipantVital
+from .models.adverse_event_model import AdverseEvent
 # Register your models here.
 
 admin.site.register(ParticipantVital)
+admin.site.register(AdverseEvent)
