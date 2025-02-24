d9fd7f8f0a195261dc658659aa1fe5c71c50c641
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 12:36:50 +0200

    refact: set max length on char fields and restructure severity choices

--- base/models/adverse_event_model.py
+++ base/models/adverse_event_model.py
@@ -3,12 +3,13 @@ from django.db import models
 
 class AdverseEvent(models.Model):
 
-    SEVERITY = ["Mild", "Moderate", "Severe"]
+    SEVERITY = (('mild', "Mild"), ('moderate', "Moderate"),
+                ('severe', "Severe"))
 
     participant_identifier = models.CharField(max_length=50)
-    severity = models.CharField(choices=SEVERITY)
+    severity = models.CharField(choices=SEVERITY, max_length=10)
     onset_date = models.DateField()
     resolution_date = models.DateField()
-    action_taken = models.CharField()
-    outcome = models.CharField()
-    investigator_review = models.CharField()
+    action_taken = models.CharField(max_length=500)
+    outcome = models.CharField(max_length=500)
+    investigator_review = models.CharField(max_length=500)
