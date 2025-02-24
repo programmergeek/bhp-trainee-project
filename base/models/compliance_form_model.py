d8e3bbaa9f3cd0429fafca51bc4af0dfa156f498
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 14:50:13 +0200

    fix: define max length

--- base/models/compliance_form_model.py
+++ base/models/compliance_form_model.py
@@ -4,7 +4,7 @@ from django.db import models
 class ComplianceForm(models.Model):
 
     participant_identifier = models.CharField(max_length=50)
-    dose_taken = models.CharField()
-    dose_prescribed = models.CharField()
-    participant_feedback = models.CharField()
-    investigator_comment = models.CharField()
+    dose_taken = models.CharField(max_length=100)
+    dose_prescribed = models.CharField(max_length=100)
+    participant_feedback = models.CharField(max_length=500)
+    investigator_comment = models.CharField(max_length=500)
