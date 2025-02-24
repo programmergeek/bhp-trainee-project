e6cf5e4bf0a2437dc4448714e0811ba105ee8c14
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 15:56:01 +0200

    refact: field type

--- base/models/final_questionnaire_model.py
+++ base/models/final_questionnaire_model.py
@@ -7,8 +7,8 @@ class FinalQuestionnaire(models.Model):
                'Moderate'), ('very', 'Very'))
 
     participant_identifier = models.CharField(max_length=50)
-    participant_experience = models.CharField(max_length=500)
-    perceived_benefits = models.CharField(max_length=500)
-    perceived_challenges = models.CharField(max_length=500)
+    participant_experience = models.TextField(max_length=500)
+    perceived_benefits = models.TextField(max_length=500)
+    perceived_challenges = models.TextField(max_length=500)
     willingness_to_participate_in_future_studies = models.CharField(
         max_length=20, choices=CHOICES)
