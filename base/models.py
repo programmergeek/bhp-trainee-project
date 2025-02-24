09471abdab2e0f52671014c65f92468592e7cf42
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-24 10:29:43 +0200

    connect app

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from .models.blood_samples_models import BloodSamples
+from .models.adverse_event_model import AdverseEvent
+from .models.vitals_model import PaticipantVitals
+
 
 # Create your models here.
