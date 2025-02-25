bc5c7f0c706898552f9369c5d21b97cfcb9837ff
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-25 14:49:29 +0200

    feat[test]: backdating and future dating blood collection dates

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models.blood_samples_models import BloodSample
+
+
+class BloodSampleModelTest(TestCase):
+
+    # Test if you can give the collection date of blood sample to some date in the future.
+    def test_future_blood_sample_collection_date(self):
+
+        date = timezone.now() + datetime.timedelta(days=30)
+        future_sample = BloodSample(collection_date=date)
+        self.assertIs(future_sample.collection_date.date()
+                      > timezone.now().date(), False)
+
+    # Test if you can backdate a blood sample collection
+    def test_past_blood_sample_collection_date(self):
+
+        date = (timezone.now() + datetime.timedelta(days=-30)).date()
+        past_sample = BloodSample(collection_date=date)
+        self.assertIs(past_sample.collection_date <
+                      timezone.now().date(), False)
