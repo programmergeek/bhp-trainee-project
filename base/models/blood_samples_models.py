4c9e835458f444288426382eb0447da598b5e76a
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-25 14:50:46 +0200

    feat[model]: create default date for blood collection date

--- base/models/blood_samples_models.py
+++ base/models/blood_samples_models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BloodSample(models.Model):
 
     participant_identifier = models.CharField(max_length=50)
     sample_id = models.CharField(max_length=20)
-    collection_date = models.DateTimeField()
+    collection_date = models.DateTimeField(default=timezone.now())
     requested_test = models.CharField(max_length=256)
     phlebotomist_signature = models.CharField(max_length=256)
