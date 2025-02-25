0a86b0659b0ceee200e884e1b59544499665f53e
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-02-25 10:48:21 +0200

    refact: replace char fields with text fields

--- base/models/compliance_form_model.py
+++ base/models/compliance_form_model.py
@@ -6,5 +6,5 @@ class ComplianceForm(models.Model):
     participant_identifier = models.CharField(max_length=50)
     dose_taken = models.CharField(max_length=100)
     dose_prescribed = models.CharField(max_length=100)
-    participant_feedback = models.CharField(max_length=500)
-    investigator_comment = models.CharField(max_length=500)
+    participant_feedback = models.TextField()
+    investigator_comment = models.TextField()
--- base/models/exit_interview_model.py
+++ base/models/exit_interview_model.py
@@ -4,6 +4,6 @@ from django.db import models
 class ExitInterview(models.Model):
 
     participant_identifier = models.CharField(max_length=50)
-    summary_of_parricipant_experience = models.CharField(max_length=500)
-    investigator_observations = models.CharField(max_length=500)
-    final_comments_and_recommendations = models.CharField(max_length=500)
+    summary_of_parricipant_experience = models.TextField
+    investigator_observations = models.TextField()
+    final_comments_and_recommendations = models.TextField()
