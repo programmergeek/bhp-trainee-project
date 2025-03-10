2574f0ddf7073fcabe024862510b16aa2f9f9464
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-03-10 11:01:24 +0200

    feat: create subject locator models, forms and admin

--- mock_study_subjects/admin/subject_locator_admin.py
+++ mock_study_subjects/admin/subject_locator_admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from ..admin_site import mock_study_admin
-from ..fieldsets import other_indirect_contacts_fieldset, indirect_contacts_fieldset
-from ..fieldsets import subject_contacts_fieldset
 from ..forms.subject_locator_form import SubjectLocatorForm
 from ..models.subject_locator import SubjectLocator
-
-from ..fieldsets import work_contacts_fieldset
 
 
 @admin.register(SubjectLocator, site=mock_study_admin)
@@ -21,28 +17,5 @@ class SubjectLocatorAdmin(admin.ModelAdmin):
                 'home_village',
             )}),
     )
-
-    # radio_fields = {
-    #     'may_visit_home': admin.VERTICAL,
-    #     'may_call': admin.VERTICAL,
-    #     'may_sms': admin.VERTICAL,
-    #     'may_call_work': admin.VERTICAL,
-    #     'may_contact_indirectly': admin.VERTICAL}
-
-    # list_filter = (
-    #     'may_visit_home',
-    #     'may_call',
-    #     'may_sms',
-    #     'may_call_work',
-    #     'may_contact_indirectly')
-    #
-    # list_display = (
-    #     'subject_identifier',
-    #     'dashboard',
-    #     'visit_home',
-    #     'call',
-    #     'sms',
-    #     'call_work',
-    #     'contact_indirectly')
 
     search_fields = ('subject_identifier', )
