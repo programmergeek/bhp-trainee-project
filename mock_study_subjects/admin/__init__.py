eb3ea22c2c29ad143252222ff9d697da9aa9f945
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-03-12 09:10:42 +0200

    migrate: cbc blood test model migration

--- mock_study_subjects/admin/__init__.py
+++ mock_study_subjects/admin/__init__.py
@@ -5,3 +5,4 @@ from .subject_locator_admin import SubjectLocatorAdmin
 from .subject_vitals_admin import SubjectVitalsAdmin
 from .subject_blood_sample_admin import SubjectBloodSampleAdmin
 from .liver_panel_blood_test_admin import LiverPanelBloodTestAdmin
+from .cbc_blood_test_result_admin import CBCBloodTestResultAdmin
