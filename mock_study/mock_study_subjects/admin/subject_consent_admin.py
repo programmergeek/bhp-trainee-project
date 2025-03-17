f7c5c8e0d00c978b73df11ee479a5ed6b9ab9ba2
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-03-17 08:54:24 +0200

    feat: integrate edc consent module

--- mock_study/mock_study_subjects/admin/subject_consent_admin.py
+++ mock_study/mock_study_subjects/admin/subject_consent_admin.py
@@ -2,22 +2,74 @@ from django.contrib import admin
 from ..admin_site import mock_study_admin
 from ..forms.subject_consent_form import SubjectConsentForm
 from ..models.subject_consent import SubjectConsent
+from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_consent.modeladmin_mixins import ModelAdminConsentMixin
+from edc_model_admin import (
+    ModelAdminFormAutoNumberMixin, ModelAdminInstitutionMixin,
+    audit_fieldset_tuple, ModelAdminNextUrlRedirectMixin,
+    ModelAdminReplaceLabelTextMixin)
+
+
+class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminFormAutoNumberMixin,
+                      ModelAdminRevisionMixin, ModelAdminReplaceLabelTextMixin,
+                      ModelAdminInstitutionMixin):
+
+    list_per_page = 10
+    date_hierarchy = 'modified'
+    empty_value_display = '-'
 
 
 @admin.register(SubjectConsent, site=mock_study_admin)
-class SubjectConsentAdmin(admin.ModelAdmin):
+class SubjectConsentAdmin(ModelAdminConsentMixin, ModelAdminMixin, admin.ModelAdmin):
 
     form = SubjectConsentForm
 
     fieldsets = (
         (None, {
             'fields': (
+                'subject_identifier',
                 'screening_identifier',
-                'gender',)}),
+                'first_name',
+                'last_name',
+                'initials',
+                'language',
+                'is_literate',
+                'witness_name',
+                'consent_datetime',
+                'gender',
+                'dob',
+                'guardian_name',
+                'is_dob_estimated',
+                'identity',
+                'identity_type',
+                'confirm_identity',
+                'is_incarcerated')}),
         ('Review Questions', {
-            'fields': (),
+            'fields': (
+                'consent_reviewed',
+                'study_questions',
+                'assessment_score',
+                'consent_signature',
+                'consent_copy'),
             'description': 'The following questions are directed to the interviewer.'}),
-    )
+        audit_fieldset_tuple)
+
+    search_fields = ('subject_identifier', 'identity')
+
+    radio_fields = {
+        "assessment_score": admin.VERTICAL,
+        "consent_copy": admin.VERTICAL,
+        "consent_reviewed": admin.VERTICAL,
+        "consent_signature": admin.VERTICAL,
+        "gender": admin.VERTICAL,
+        "is_dob_estimated": admin.VERTICAL,
+        "is_incarcerated": admin.VERTICAL,
+        "is_literate": admin.VERTICAL,
+        "language": admin.VERTICAL,
+        "may_store_genetic_samples": admin.VERTICAL,
+        "may_store_samples": admin.VERTICAL,
+        "study_questions": admin.VERTICAL,
+    }
 
     search_fields = ('subject_identifier', 'identity')
 
--- mock_study/mock_study_subjects/models/subject_consent.py
+++ mock_study/mock_study_subjects/models/subject_consent.py
@@ -1,15 +1,107 @@
 from ..constants import GENDER
 from django.db import models
-from edc_registration.model_mixins import UpdatesOrCreatesRegistrationModelMixin
+from edc_registration.model_mixins import UpdatesOrCreatesRegistrationModelMixin as BaseUpdatesOrCreatesRegistrationModelMixin
+from edc_base.constants import DEFAULT_BASE_FIELDS
+from django.core.exceptions import ImproperlyConfigured
+from edc_base.model_managers import HistoricalRecords
+from edc_base.model_mixins import BaseUuidModel
+from edc_base.sites import CurrentSiteManager
+from edc_base.sites.site_model_mixin import SiteModelMixin
+from edc_consent.field_mixins import (SampleCollectionFieldsMixin,
+                                      CitizenFieldsMixin)
+from edc_consent.field_mixins import IdentityFieldsMixin
+from edc_consent.field_mixins import ReviewFieldsMixin, PersonalFieldsMixin
+from edc_consent.field_mixins import VulnerabilityFieldsMixin
+from edc_consent.managers import ConsentManager as SubjectConsentManager
+from edc_consent.model_mixins import ConsentModelMixin
+from edc_identifier.model_mixins import UniqueSubjectIdentifierModelMixin
+from edc_registration.model_mixins import (
+    UpdatesOrCreatesRegistrationModelMixin)
+from ..subject_identifier import SubjectIdentifier
 
 """
-NOTES: 
-
+NOTES:
  - UpdatesOrCreatesRegistrationModelMixin: handles registering a participant
 """
 
 
-class SubjectConsent(models.Model):
+class ConsentManager(models.Manager):
+
+    '''
+    This function allows us to search for a subject's consent using their subject identifier and the version of the consent form
+    '''
+
+    def get_by_natural_key(self, subject_identifier, version):
+        return self.get(
+            subject_identifier=subject_identifier, version=version)
+
+# A mixin is a reusable class that contains useful pieces of code I can use in different parts of my app.
+
+
+class updatesOrCreatesRegistrationMixin(BaseUpdatesOrCreatesRegistrationModelMixin):
+
+    # This is a property inherited from the super class
+    # It returns the attribute that will update registered_subject_unique_field
+    @property
+    def registration_unique_field(self):
+        return 'subject_identifier'
+
+    # gets values for attributes that are shared between the registration model and this instance
+    @property
+    def registration_options(self):
+        registration_options = {}
+        for field in self.registration_model._meta.get_fields():
+            # _state is an internal value used by django to track the lifecycle of the model instance
+            if (field.name not in DEFAULT_BASE_FIELDS + ['_state']
+                    + [self.registration_unique_field]):
+                try:
+                    registration_options.update({field.name: getattr(
+                        self, field.name)})
+                except AttributeError:
+                    pass
+        return registration_options
+
+    def registration_raise_on_not_unique(self):
+        '''
+        This field check if the value of registration_identifier is unique.
+        registration_identifier is equal to the return value of
+        registration_unique_field which is subject_identifier
+        '''
+
+        unique_fields = ['registration_identifier']
+        for f in self.registration_model._meta.get_fields():
+            try:
+                if f.unique:
+                    unique_fields.append(f.name)
+            except AttributeError:
+                pass
+        if self.registration_unique_field not in unique_fields:
+            raise ImproperlyConfigured(
+                'Field is not unique. Got {}-- {}'.format(
+                    self._meta.label_lower, self.registration_unique_field))
+
+    class Meta:
+        # Defines this mixin as an abstract model, meaning only the models which implement this mixin
+        # have tables created for them in the database, but the mixin won't get a table
+        abstract = True
+
+
+class SubjectConsent(ConsentModelMixin,  # contains common properties for consent models
+                     # keeps track of which site the consent is giver and the particpant is registered
+                     SiteModelMixin,
+                     # contains logic to register participant
+                     UpdatesOrCreatesRegistrationModelMixin,
+                     # Creating a subject identifier and using that as
+                     # creates a unique subject identifier field and its corresponding methods
+                     UniqueSubjectIdentifierModelMixin,
+                     IdentityFieldsMixin,  # fields related to persons identity e.g id number
+                     ReviewFieldsMixin,  # Review questions
+                     PersonalFieldsMixin,
+                     SampleCollectionFieldsMixin,
+                     # Fields relating to a person's citizenship status or if they are married to citizen.
+                     CitizenFieldsMixin,
+                     VulnerabilityFieldsMixin,
+                     BaseUuidModel):
 
     subject_screening_model = 'mock_study_subjects.subject_screening'
 
@@ -28,12 +120,23 @@ class SubjectConsent(models.Model):
 
     is_signed = models.BooleanField(default=False, editable=False)
 
+    consent = SubjectConsentManager()
+
+    objects = ConsentManager()
+
+    history = HistoricalRecords()
+
+    on_site = CurrentSiteManager()
+
     def __str__(self):
         return f'{self.subject_identifier} V{self.version}'
 
     def save(self, *args, **kwargs):
         self.subject_type = 'subject'
         super().save(*args, **kwargs)
+
+    def natural_key(self):
+        return (self.subject_identifier, self.version,)
 
     class Meta:
         app_label = 'mock_study_subjects'
