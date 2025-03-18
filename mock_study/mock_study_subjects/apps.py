36339890bfc7eddf0d18c31f78c951573420c745
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-03-18 07:59:00 +0200

    remove upper and lower bound of participant

--- mock_study/mock_study_subjects/apps.py
+++ mock_study/mock_study_subjects/apps.py
@@ -10,9 +10,6 @@ class AppConfig(DjangoApponfig):
     verbose_name = 'Mock Study Subject CRFs'
     admin_site_name = 'mock_study_subject_admin'
 
-    screening_age_adult_upper = 99
-    screening_age_adult_lower = 18
-
 
 class EdcTimepointAppConfig(BaseEdcTimepointAppConfig):
     timepoints = TimepointCollection(
