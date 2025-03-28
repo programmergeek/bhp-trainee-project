d736a52f6fb5739e8cbc9952a3eec9c7593a8cae
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-03-28 09:08:34 +0200

    refact: use edc validators instead

--- mock_study/mock_study_subject_validations/form_validations/subject_screening_validator.py
+++ mock_study/mock_study_subject_validations/form_validations/subject_screening_validator.py
@@ -1,33 +1,19 @@
-from django import forms
 from edc_form_validators import FormValidator
-from edc_constants.constants import MALE, YES
+from edc_constants.constants import MALE
 
 
 class SubjectScreeningValidator(FormValidator):
 
     def clean(self):
         super().clean()
-        self.clean_male_pregnant_breastfeeding()
+        self.applicable_if_true(
+            field='gender',
+            condition=self.cleaned_data.get('gender') != MALE,
+            field_applicable='is_pregnant'
+        )
 
-    def clean_male_pregnant_breastfeeding(self):
-
-        data = self.cleaned_data
-        gender = data.get('gender')
-        is_pregnant = data.get('is_pregnant')
-        is_breastfeeding = data.get('is_breastfeeding')
-
-        if (is_breastfeeding == YES and is_pregnant == YES and gender == MALE):
-            raise forms.ValidationError({
-                'is_pregnant': 'Male subjects cannot be pregnant.',
-                'is_breastfeeding': 'Male subjectes cannot breastfeed.'
-            })
-
-        elif (is_pregnant == YES and gender == MALE):
-            raise forms.ValidationError({
-                'is_pregnant': 'Male subjects cannot be pregnant.'
-            })
-
-        elif (is_breastfeeding == YES and gender == MALE):
-            raise forms.ValidationError({
-                'is_breastfeeding': 'Male subjectes cannot breastfeed.'
-            })
+        self.applicable_if_true(
+            field='gender',
+            condition=self.cleaned_data.get('gender') != MALE,
+            field_applicable='is_breastfeeding'
+        )
