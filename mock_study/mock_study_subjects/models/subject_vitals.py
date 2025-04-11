78827795a991703d6eb103acb3c7143390cf3039
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-04-11 09:04:37 +0200

    feat: add validations

--- mock_study/mock_study_subjects/models/subject_vitals.py
+++ mock_study/mock_study_subjects/models/subject_vitals.py
@@ -1,35 +1,56 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from .mixins.crfs_model_mixin import CrfModelMixin
 
 
 class SubjectVitals(CrfModelMixin):
     systolic_blood_pressure = models.IntegerField(
         help_text='mmHg',
-        default=0
+        default=0,
+        validators=[
+            MaxValueValidator(250),
+            MinValueValidator(50)
+        ]
     )
 
     diastolic_blood_pressure = models.IntegerField(
         help_text='mmHg',
-        default=0
+        default=0,
+        validators=[
+            MaxValueValidator(250),
+            MinValueValidator(50)
+        ]
     )
 
     heart_rate = models.IntegerField(
         help_text='bpm',
-        default=0
+        default=0,
+        validators=[
+            MaxValueValidator(200),
+            MinValueValidator(30)
+        ]
     )
 
     height = models.DecimalField(
         max_digits=4,
         decimal_places=1,
         help_text='cm',
-        default=0
+        default=0,
+        validators=[
+            MaxValueValidator(300),
+            MinValueValidator(50)
+        ]
     )
 
     weight = models.DecimalField(
         max_digits=4,
         decimal_places=1,
         help_text='kg',
-        default=0
+        default=0,
+        validators=[
+            MaxValueValidator(500),
+            MinValueValidator(30)
+        ]
     )
 
     class Meta:
