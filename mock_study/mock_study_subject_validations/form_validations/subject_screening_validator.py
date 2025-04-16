f6aea297aa608b025f612681b69f1ca06e8ee272
Thato Will Joseph <Will@Sams-MacBook-Pro.local>
2025-04-16 10:37:22 +0200

    incomplete fix screening validation

--- mock_study/mock_study_subject_validations/form_validations/subject_screening_validator.py
+++ mock_study/mock_study_subject_validations/form_validations/subject_screening_validator.py
@@ -1,5 +1,5 @@
 from edc_form_validators import FormValidator
-from edc_constants.constants import MALE
+from edc_constants.constants import FEMALE
 
 
 class SubjectScreeningValidator(FormValidator):
@@ -7,12 +7,12 @@ class SubjectScreeningValidator(FormValidator):
     def clean(self):
         self.applicable_if_true(
             field='gender',
-            condition=self.cleaned_data.get('gender') != MALE,
+            condition=self.cleaned_data.get('gender') == FEMALE,
             field_applicable='is_pregnant'
         )
 
         self.applicable_if_true(
             field='gender',
-            condition=self.cleaned_data.get('gender') != MALE,
+            condition=self.cleaned_data.get('gender') == FEMALE,
             field_applicable='is_breastfeeding'
         )
