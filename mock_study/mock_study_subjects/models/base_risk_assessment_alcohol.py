1893843dad8e931d038fb3a64f1fbf4187799f80
Will Joseph <programergeek@gmail.com>
2025-05-14 15:37:07 +0200

    fix: validator

--- mock_study/mock_study_subjects/models/base_risk_assessment_alcohol.py
+++ mock_study/mock_study_subjects/models/base_risk_assessment_alcohol.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from edc_constants.choices import YES_NO
 
@@ -10,9 +10,9 @@ class BaseRiskAssessmentAlcohol(CrfModelMixin):
 
     consumes_alcohol = models.CharField(max_length=3, choices=YES_NO , verbose_name='Do you drink alcohol?')
 
-    drinks_per_week = models.IntegerField(verbose_name='How many days per week do you consume alcohol?', validators=[MinValueValidator(1), MaxLengthValidator(7)], blank=True, null=True)
+    drinks_per_week = models.IntegerField(verbose_name='How many days per week do you consume alcohol?', validators=[MinValueValidator(1), MaxValueValidator(7)], blank=True, null=True)
 
-    amount_of_drinks = models.IntegerField(verbose_name='When you do drink, how many drinks do you normally have?', validators=[MinValueValidator(1), MaxLengthValidator(30)], blank=True, null=True)
+    amount_of_drinks = models.IntegerField(verbose_name='When you do drink, how many drinks do you normally have?', validators=[MinValueValidator(1), MaxValueValidator(30)], blank=True, null=True)
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'mock_study_subjects'
